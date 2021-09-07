c85dce62570f45aa7917cc66974eb3ded325b8ae
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2021-09-07 21:01:51 +0200

    initial commit

---
+++ mini-project-expanding-search/main.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    container.classList.toggle('active');
+    input.focus();
+    console.log('radi')
+});
