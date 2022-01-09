17ccfd6b0f50259737f87922a59a27f66d079272
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-01-09 22:37:22 +0100

    added hamburger menu

--- travel-agency/main.js
+++ travel-agency/main.js
@@ -6,3 +6,14 @@ function fixNav() {
         header.classList.add('active')
         : header.classList.remove('active');
 }
+
+const btn = document.getElementById('menu-btn');
+// const nav = document.getElementById('menu');
+
+function navigation() {
+    btn.classList.toggle('open');
+    // nav.classList.toggle('hidden');
+    document.body.classList.toggle('no-scroll');
+}
+
+btn.addEventListener('click', navigation);
