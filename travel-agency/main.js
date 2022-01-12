7780697e586e83a206f9d987ddf7799a5e76ae2b
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-01-12 21:34:29 +0100

    fixed headers box-shadow

--- travel-agency/main.js
+++ travel-agency/main.js
@@ -10,14 +10,15 @@ function fixNav() {
 const btn = document.getElementById('menu-btn');  //hamburger icon
 const nav = document.getElementById('menu');      //mobile menu container
 
-function mobileNav() {
-    header.classList.add('active')
+function mobileMenuOpen() {
+    header.classList.add('active');
+    header.classList.add('no-box-shadow');
     btn.classList.toggle('open');
     nav.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll');
 }
 
-btn.addEventListener('click', mobileNav);
+btn.addEventListener('click', mobileMenuOpen);
 
 const mobileMenuItem = document.querySelectorAll('.mobile-nav-item');
 
@@ -25,6 +26,7 @@ function mobileMenuClose() {
     btn.classList.toggle('open');
     nav.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll');
+    header.classList.remove('no-box-shadow');
 
     //popraviti - kada se klikne na prvi element da nestane i pozadinska boja header containera
 }
