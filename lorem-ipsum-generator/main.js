6d188bbcee547e9726cfa94f65ca833f9f441120
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-02-11 20:06:28 +0100

    async/await func fixes

--- lorem-ipsum-generator/main.js
+++ lorem-ipsum-generator/main.js
@@ -2,30 +2,41 @@ const form = document.querySelector(".lorem-form");
 const ammount = document.getElementById("ammount");
 const result = document.querySelector(".lorem-text");
 
-let text = [];
+let text = [
+    "Cornhole gluten-free bitters street art semiotics, listicle chicharrones put a bird on it. Squid YOLO fanny pack roof party.  Edison bulb beard ennui freegan, craft beer gastropub master cleanse 90's organic. Vexillologist cloud bread tote bag kale chips fashion axe. VHS tumeric selvage everyday carry, distillery helvetica bitters wolf kombucha shoreditch whatever. Synth pork belly readymade godard, kinfolk jianbing kale chips wolf prism.",
+];
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
     // prevents submision to the server when user clicks on submit button
     e.preventDefault();
 
+    // gets num of paragraphs and creates API url
     const value = parseInt(ammount.value);
-
     let url = `https://hipsum.co/api/?type=hipster-centric&paras=${value}`;
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            text = Object.values(data);
-            console.log(typeof text);
-        });
+    async function fetchText() {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    console.log(text.length);
+        let myJSON = JSON.stringify(data);
+        let jsonData = JSON.parse(myJSON);
 
-    const random = Math.floor(Math.random() * text.length);
+        text = Object.values(jsonData);
+        // console.log(typeof text, text);
+    }
+
+    await fetchText();
+
+    // fetch(url)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         text = Object.values(data);
+    //         console.log(typeof text);
+    //     });
 
     if (isNaN(value) || value < 1) {
-        result.innerHTML = `<p class="result">${text[random]}</p>`;
+        result.innerHTML = `<p class="result">${text[0]}</p>`;
     } else {
         let tempText = text.slice(0, value);
         tempText = tempText
@@ -33,6 +44,7 @@ form.addEventListener("submit", function (e) {
                 return `<p class="result">${item}</p>`;
             })
             .join("");
+        // console.log(tempText);
         result.innerHTML = tempText;
     }
 });
