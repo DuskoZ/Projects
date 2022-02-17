4e069e98bc19fb5886ea8b09c32e671ecd9bb73d
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-02-18 00:12:33 +0100

    added js solution for different text types

--- lorem-ipsum-generator/main.js
+++ lorem-ipsum-generator/main.js
@@ -1,19 +1,35 @@
 const form = document.querySelector(".lorem-form");
 const ammount = document.getElementById("ammount");
 const result = document.querySelector(".lorem-text");
+const numLabel = document.getElementById("num");
 
 let text = [
     "Cornhole gluten-free bitters street art semiotics, listicle chicharrones put a bird on it. Squid YOLO fanny pack roof party.  Edison bulb beard ennui freegan, craft beer gastropub master cleanse 90's organic. Vexillologist cloud bread tote bag kale chips fashion axe. VHS tumeric selvage everyday carry, distillery helvetica bitters wolf kombucha shoreditch whatever. Synth pork belly readymade godard, kinfolk jianbing kale chips wolf prism.",
 ];
 
+// changes label value between "Num of paragraphs:" and "Num of sentences:"
+const radios = document.querySelectorAll('input[type="radio"]');
+radios.forEach((radio) => {
+    radio.addEventListener("click", (e) => {
+        numLabel.innerHTML = `Num of ${e.target.value}s:`;
+    });
+});
+
 form.addEventListener("submit", async function (e) {
     // prevents submision to the server when user clicks on submit button
     e.preventDefault();
 
+    // gets user's choice - paragraph or sentence - and sets variable which will be used in API url
+    const type = document.querySelector('input[name="type"]:checked').value;
+    let typeOfText = "";
+    type === "paragraph" ? (typeOfText = "paras") : (typeOfText = "sentences");
+
     // gets num of paragraphs and creates API url
     const value = parseInt(ammount.value);
-    let url = `https://hipsum.co/api/?type=hipster-centric&paras=${value}`;
+    let url = `https://hipsum.co/api/?type=hipster-centric&${typeOfText}=${value}`;
+    // console.log(url);
 
+    // gets text from API
     async function fetchText() {
         const response = await fetch(url);
         const data = await response.json();
@@ -44,7 +60,13 @@ form.addEventListener("submit", async function (e) {
                 return `<p class="result">${item}</p>`;
             })
             .join("");
-        // console.log(tempText);
+
         result.innerHTML = tempText;
     }
 });
+
+// v0.1
+// added basic API call which gets desired num of paragraphs
+
+// v0.2
+// added choice between paragraphs and sentences
