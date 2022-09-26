f34d27cbf065899515d896088d8c9f3c8d2f9332
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-09-26 17:17:04 +0200

    updated script

---
+++ rick-and-morty/main.js
@@ -0,0 +1,29 @@
+const anchor = document.getElementById("anchor");
+const eyes = document.querySelectorAll(".eye");
+const rect = anchor.getBoundingClientRect();
+
+const anchorX = rect.left + rect.width / 2;
+const anchorY = rect.top + rect.height / 2;
+
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+    // console.log(angleDeg);
+
+    eyes.forEach((eye) => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+        anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+    });
+});
+
+function angle(cx, cy, ex, ey) {
+    const dy = ey - cy;
+    const dx = ex - cx;
+
+    const rad = Math.atan(dy, dx);
+    const deg = (rad * 180) / Math.PI;
+
+    return deg;
+}
