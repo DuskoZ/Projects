63feba4b25c0c3639d2a47c4d8401c283377299f
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-11-18 20:34:13 +0100

    updated js

--- parallax-scroll-effect/main.js
+++ parallax-scroll-effect/main.js
@@ -8,3 +8,37 @@ var scrollSensitivitySetting = 30; //Increase/decrease this number to change sen
 var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
+
+function parallaxScroll(evt) {
+    if (isFirefox) {
+        //Set delta for Firefox
+        delta = evt.detail * -120;
+    } else if (isIe) {
+        //Set delta for IE
+        delta = -evt.deltaY;
+    } else {
+        //Set delta for all other browsers
+        delta = evt.wheelDelta;
+    }
+
+    if (ticking != true) {
+        if (delta <= -scrollSensitivitySetting) {
+            //Down scroll
+            ticking = true;
+            if (currentSlideNumber !== totalSlideNumber - 1) {
+                currentSlideNumber++;
+                nextItem();
+            }
+            slideDurationTimeout(slideDurationSetting);
+        }
+        if (delta >= scrollSensitivitySetting) {
+            //Up scroll
+            ticking = true;
+            if (currentSlideNumber !== 0) {
+                currentSlideNumber--;
+            }
+            previousItem();
+            slideDurationTimeout(slideDurationSetting);
+        }
+    }
+}
