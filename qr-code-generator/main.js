36962988ae652df74c3fff527029eccb1b22939d
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-02-28 21:44:06 +0100

    updated js part

--- qr-code-generator/main.js
+++ qr-code-generator/main.js
@@ -1,7 +1,8 @@
 const wrapper = document.querySelector(".container"),
     qrInput = wrapper.querySelector(".form input"),
     generateBtn = wrapper.querySelector(".form button"),
-    qrImg = wrapper.querySelector(".qr-code img");
+    qrImg = wrapper.querySelector(".qr-code img"),
+    downloadBtn = wrapper.querySelector(".download-btn");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
@@ -14,6 +15,28 @@ generateBtn.addEventListener("click", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
     });
+});
+
+downloadBtn.addEventListener("click", () => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const image = new Image();
+
+    image.onload = function () {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        context.drawImage(image, 0, 0);
+
+        // Trigger download
+        const a = document.createElement("a");
+        a.href = canvas.toDataURL("image/png");
+        a.download = "qr-code.png";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    };
+
+    image.src = qrImg.src;
 });
 
 qrInput.addEventListener("keyup", () => {
