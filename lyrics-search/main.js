5ef7cb9f0d989f5b345bd97ccd6390b4c9c780a0
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-03-19 17:32:37 +0100

    updated js

--- lyrics-search/main.js
+++ lyrics-search/main.js
@@ -39,3 +39,28 @@ async function getLyrics(artist, songTitle) {
 
     more.innerHTML = "";
 }
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if (!searchTerm) {
+        alert("Please type in a search term");
+    } else {
+        searchSongs(searchTerm);
+    }
+});
+
+// Get lyrics button click
+result.addEventListener("click", (e) => {
+    const clickedEl = e.target;
+
+    if (clickedEl.tagName === "BUTTON") {
+        const artist = clickedEl.getAttribute("data-artist");
+        const songTitle = clickedEl.getAttribute("data-songtitle");
+
+        getLyrics(artist, songTitle);
+    }
+});
