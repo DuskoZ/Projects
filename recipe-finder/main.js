3d947cac4be829eee094bded4b8cd605fdb68e44
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-03-31 22:04:08 +0200

    updated js

--- recipe-finder/main.js
+++ recipe-finder/main.js
@@ -46,3 +46,29 @@ function searchMeal(e) {
         alert("Please enter a search term");
     }
 }
+
+// Fetch meal by ID
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        });
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = "";
+    resultHeading.innerHTML = "";
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then((res) => res.json())
+        .then((data) => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        });
+}
