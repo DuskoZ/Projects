916ecea3f8440d83aa10eca80fb80e2d54690471
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-04-01 22:41:06 +0200

    updated js

--- recipe-finder/main.js
+++ recipe-finder/main.js
@@ -72,3 +72,22 @@ function getRandomMeal() {
             addMealToDOM(meal);
         });
 }
+
+// Event listeners
+submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
+
+mealsEl.addEventListener("click", (e) => {
+    const mealInfo = e.composedPath().find((item) => {
+        if (item.classList) {
+            return item.classList.contains("meal-info");
+        } else {
+            return false;
+        }
+    });
+
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute("data-mealid");
+        getMealById(mealID);
+    }
+});
