46683826f90386f7bea458fb1bf60dc0969090bf
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-08-12 19:33:44 +0200

    updated js

--- progress-sidenav/main.js
+++ progress-sidenav/main.js
@@ -44,3 +44,39 @@ function drawPath() {
         item.pathEnd = navPath.getTotalLength();
     });
 }
+
+function syncPath() {
+    const someElsAreVisible = () =>
+            nav.querySelectorAll(`.${visibleClass}`).length > 0,
+        thisElIsVisible = (el) => el.classList.contains(visibleClass),
+        pathLength = navPath.getTotalLength();
+
+    let pathStart = pathLength,
+        pathEnd = 0,
+        lastPathStart,
+        lastPathEnd;
+
+    navItems.forEach((item) => {
+        if (thisElIsVisible(item.listItem)) {
+            pathStart = Math.min(item.pathStart, pathStart);
+            pathEnd = Math.max(item.pathEnd, pathEnd);
+        }
+    });
+
+    if (someElsAreVisible() && pathStart < pathEnd) {
+        if (pathStart !== lastPathStart || pathEnd !== lastPathEnd) {
+            const dashArray = `1 ${pathStart} ${
+                pathEnd - pathStart
+            } ${pathLength}`;
+
+            navPath.style.setProperty("stroke-dashoffset", "1");
+            navPath.style.setProperty("stroke-dasharray", dashArray);
+            navPath.style.setProperty("opacity", 1);
+        }
+    } else {
+        navPath.style.setProperty("opacity", 0);
+    }
+
+    lastPathStart = pathStart;
+    lastPathEnd = pathEnd;
+}
