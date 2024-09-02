0a1376add3f716e2625f2dc1ace73926005df5b7
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-09-02 17:34:11 +0200

    updated JS

---
+++ context-menu/main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+let menuVisible = false;
+
+const toggleMenu = (command) => {
+    menu.style.display = command === "show" ? "block" : "none";
+    menuVisible = !menuVisible;
+};
+
+const setPosition = ({ top, left }) => {
+    menu.style.left = `${left}px`;
+    menu.style.top = `${top}px`;
+    toggleMenu("show");
+};
