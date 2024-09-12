c7229399f7d30b64ccc0e51af8c77bb32f81563b
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-09-12 15:56:02 +0200

    updated JS

---
+++ menu-reveal-animation/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const revealerNav = window.revealer({
+        revealElementSelector: ".nav-js",
+        options: {
+            anchorSelector: ".nav-btn-js",
+        },
+    });
+});
