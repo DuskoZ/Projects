18cb5f5d186d68f347a3834f5eb0f246201e2086
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-10-18 21:50:59 +0200

    updated js

---
+++ menu-animation/main.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu-btn");
+const topLeftSlider = document.querySelector(".top-left-slide");
+const bottomLeftSlider = document.querySelector(".bottom-left-slide");
+
+const topRightSlider = document.querySelector(".top-right-slide");
+const middleRightSlider = document.querySelector(".middle-right-slide");
+const bottomRightSlider = document.querySelector(".bottom-right-slide");
+
+const eksOne = document.querySelector(".eks-one");
+const eksTwo = document.querySelector(".eks-two");
+const eksThree = document.querySelector(".eks-three");
