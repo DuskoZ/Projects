b9644bbcfa29aa502d2d33736a48691f477428e9
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2024-10-22 15:37:22 +0200

    script updated

--- menu-animation/main.js
+++ menu-animation/main.js
@@ -9,3 +9,19 @@ const bottomRightSlider = document.querySelector(".bottom-right-slide");
 const eksOne = document.querySelector(".eks-one");
 const eksTwo = document.querySelector(".eks-two");
 const eksThree = document.querySelector(".eks-three");
+
+var isOpen = false;
+
+menu.addEventListener("click", () => {
+    topLeftSlider.classList.toggle("top-left-slide-show");
+    bottomLeftSlider.classList.toggle("bottom-left-slide-show");
+    topRightSlider.classList.toggle("top-right-slide-show");
+    middleRightSlider.classList.toggle("middle-right-slide-show");
+    bottomRightSlider.classList.toggle("bottom-right-slide-show");
+    eksTwo.classList.toggle("eks-two-fade");
+
+    const tl = gsap.timeline();
+    const tlEksThree = gsap.timeline();
+
+    isOpen = !isOpen;
+});
