27d92838f0ac57115c03605bd10fa1ad5888ed24
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-01-10 14:45:03 +0100

    updated JS

--- flipcards/main.js
+++ flipcards/main.js
@@ -15,3 +15,29 @@ document.querySelectorAll(".flip-card-click").forEach(function (item) {
         this.classList.toggle("flipped");
     });
 });
+
+// shuffle cards
+function shuffle(id) {
+    var container = document.getElementById(id);
+    var cardClass = container.getElementsByTagName("div")[0].className;
+    var elementsArray = Array.prototype.slice.call(
+        container.getElementsByClassName(cardClass)
+    );
+    elementsArray.forEach(function (element) {
+        container.removeChild(element);
+    });
+    shuffleArray(elementsArray);
+    elementsArray.forEach(function (element) {
+        container.appendChild(element);
+    });
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
