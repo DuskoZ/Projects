862b517204f311ce04ef075a8e0bf9d39a824a16
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-02-07 22:48:01 +0100

    JS update

---
+++ hero-animation/main.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", function () {
+    init();
+});
+
+function init() {
+    let toolTimeline = new TimelineMax();
+    let duration = 0.9;
+    toolTimeline.from(
+        "h1",
+        duration,
+        {
+            opacity: 0,
+            // scale:10,
+            ease: Linear.easeIn,
+        },
+        0.8
+    );
+    toolTimeline.staggerFrom(
+        ".boxes-box",
+        duration,
+        {
+            opacity: 0,
+            scale: 0,
+            ease: Back.easeInOut,
+        },
+        0.1,
+        0.2
+    );
+
+    toolTimeline.to(
+        ".boxes img",
+        duration,
+        {
+            opacity: 1,
+            right: 0,
+            ease: Back.easeOut,
+        },
+        0.4
+    );
+
+    toolTimeline.staggerTo(
+        "li",
+        duration,
+        {
+            top: 0,
+            ease: Back.easeOut,
+        },
+        0.1,
+        0.9
+    );
+}
