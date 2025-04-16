41442595f1c7a22ebe709f5b2dffecd578b05b94
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-04-16 17:55:29 +0200

    JS code

--- map-osm-leaflet/main.js
+++ map-osm-leaflet/main.js
@@ -1,1 +1,7 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView([44.816441, 20.460208], 14);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 14,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
