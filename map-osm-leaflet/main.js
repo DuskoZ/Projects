dce56dbbbf8910b49d4d22b35968b1627c20116e
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-04-17 14:02:25 +0200

    updated JS part

--- map-osm-leaflet/main.js
+++ map-osm-leaflet/main.js
@@ -1,7 +1,30 @@
 var map = L.map("map").setView([44.816441, 20.460208], 14);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 14,
+    maxZoom: 19,
     attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+navigator.geolocation.watchPosition(success, error);
+
+function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const accuracy = position.coords.accuracy;
+
+    let marker = L.marker([lat, lon]).addTo(map);
+    let circle = L.circle([lat, lon], { radius: accuracy }).addTo(map);
+
+    // map.setView([lat, lon], 14);
+    map.flyTo([lat, lon], 14, { animate: true, duration: 1 });
+    marker.bindPopup("You are here").openPopup();
+}
+
+function error(err) {
+    if (err.code == 1) {
+        alert("Please allow location access to use this feature.");
+    } else {
+        alert("Unable to retrieve your location.");
+    }
+}
