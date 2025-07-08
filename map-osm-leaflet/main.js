22d769c8092abc724d6f36a544670d4b58a5d41d
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2025-07-08 20:20:43 +0200

    onMapClick func update

--- map-osm-leaflet/main.js
+++ map-osm-leaflet/main.js
@@ -65,8 +65,19 @@ function error(err) {
     }
 }
 
+let popup = L.popup();
+
+/* 
 function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
+}
+*/
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
 }
 
 map.on("click", onMapClick);
